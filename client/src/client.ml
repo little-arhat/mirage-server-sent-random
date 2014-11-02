5ccb04824df60c9a6cedefdcc973ac604721dc68
Roma Sokolov <sokolov.r.v@gmail.com>
2014-11-03 00:55:26 +0300

    [add] simple version of client: set innerHTML

--- client/src/client.ml
+++ client/src/client.ml
@@ -1,7 +1,35 @@
 
 module Html = Dom_html
 
-let start _:(bool Js.t) = Js._false
+(* var CommentBox = React.createClass({displayName: 'CommentBox', *)
+(*   render: function() { *)
+(*     return ( *)
+(*       React.createElement('div', {className: "commentBox"}, *)
+(*         "Hello, world! I am a CommentBox." *)
+(*       ) *)
+(*     ); *)
+(*   } *)
+(* }); *)
+(* React.render( *)
+(*   React.createElement(CommentBox, null), *)
+(*   document.getElementById('content') *)
+(* ); *)
+
+(* let react s = Js.Unsafe.fun_call *)
+(*   (Unsafe.variable "React") [|Unsafe.inject (Js.string s)|] *)
+
+let on_message event =
+  let div = Dom_html.getElementById "main-area" in
+  let data = event##data in
+  let () = Firebug.console##log_2(Js.string "Got event: ", data) in
+  let _ = div##innerHTML <- data in
+  Js._false
+
+
+let start _:(bool Js.t) =
+  let evs = jsnew EventSource.eventSource (Js.string "/events") in
+  let _ = evs##onmessage <- (Dom.handler on_message) in
+  Js._false
 
 let () =
-  Html.window##onload <- Html.handler start
+  Html.window##onload <- Dom.handler start
